a09f86ce17057e0476b27ca3729391f80ce528d1
Fengda HUANG <h@phodal.com>
2015-08-21 10:11:56 +0800

    Add reply list

--- app.js
+++ app.js
@@ -1,45 +1,65 @@
 var express = require('express');
+var path = require('path');
 var wechat = require('wechat');
 var config = {
-  token: 'xuntaphodal',
-  appid: 'appid',
-  encodingAESKey: 'H8mMJW8Oz6SedEY3OiY0kQU8PTa1PDbKJN4hZEGEovR'
+    token: 'xuntaphodal',
+    appid: 'appid',
+    encodingAESKey: 'H8mMJW8Oz6SedEY3OiY0kQU8PTa1PDbKJN4hZEGEovR'
 };
 
 var app = exports.app = express();
 app.use(express.query());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/weixin', wechat(config, function (req, res, next) {
-  var message = req.weixin;
-  if (message.FromUserName === 'diaosi') {
-    res.reply('hehe');
-  } else if (message.FromUserName === 'text') {
-    res.reply({
-      content: 'text object',
-      type: 'text'
-    });
-  } else if (message.FromUserName === 'hehe') {
-    res.reply({
-      type: "music",
-      content: {
-        title: "来段音乐吧",
-        description: "一无所有",
-        musicUrl: "http://mp3.com/xx.mp3",
-        hqMusicUrl: "http://mp3.com/xx.mp3",
-        thumbMediaId: "thisThumbMediaId"
-      }
-    });
-  } else {
-    res.reply([
-      {
-        title: '你来我家接我吧',
-        description: '这是女神与高富帅之间的对话',
-        picurl: 'http://nodeapi.cloudfoundry.com/qrcode.jpg',
-        url: 'http://nodeapi.cloudfoundry.com/'
-      }
-    ]);
-  }
+    var message = req.weixin;
+    if (message.FromUserName === 'diaosi') {
+        res.reply('hehe');
+    } else if (message.FromUserName === 'text') {
+        res.reply({
+            content: 'text object',
+            type: 'text'
+        });
+    } else if (message.FromUserName === 'hehe') {
+        res.reply({
+            type: "music",
+            content: {
+                title: "来段音乐吧",
+                description: "一无所有",
+                musicUrl: "http://mp3.com/xx.mp3",
+                hqMusicUrl: "http://mp3.com/xx.mp3",
+                thumbMediaId: "thisThumbMediaId"
+            }
+        });
+    } else {
+        res.reply([
+            {
+                title: '我要捐',
+                description: '我要捐',
+                picurl: 'http://weixin.phodal.com/avatar.jpg',
+                url: 'http://nx2.phodal.com/create'
+            },
+            {
+                title: '我需要',
+                description: '我需要',
+                picurl: 'http://weixin.phodal.com/avatar.jpg',
+                url: 'http://nx2.phodal.com/need'
+            },
+            {
+                title: '已捐赠',
+                description: '已捐赠',
+                picurl: 'http://weixin.phodal.com/avatar.jpg',
+                url: 'http://nx2.phodal.com/search-needs'
+            },
+            {
+                title: '搜索',
+                description: '搜索',
+                picurl: 'http://weixin.phodal.com/avatar.jpg',
+                url: 'http://nx2.phodal.com/search-needs'
+            }
+        ]);
+    }
 }));
 
 app.listen(3080, function () {
-    console.log("http server run on http://localhost:8899");
-  });
+    console.log("http server run on http://localhost:3080");
+});
